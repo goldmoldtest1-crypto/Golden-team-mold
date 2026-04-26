25267e525bd75bee406eb23d778a50d6eeda5b35
goldmoldtest1-crypto <goldmoldtest1@gmail.com>
2026-04-26 12:32:18 -0700

    Fix: Adding TanStack Start plugin and tsconfig for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
-    tsconfigPaths(),
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart(),
+    viteReact(),
   ],
 })
